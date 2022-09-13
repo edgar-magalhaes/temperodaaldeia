d7227d3856fae2fd28e6c1ddb4930118ff944e74
Edgar Magalhães <79928270+ecmdev@users.noreply.github.com>
2022-09-13 09:34:30 -0300

    melhorias

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,26 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/merchants', function () {
+    return response()->json([
+        'status' => 'success',
+        'merchants' => [
+            [
+                'id' => 1,
+                'name' => 'Merchant 1'
+            ],
+            [
+                'id' => 2,
+                'name' => 'Merchant 2'
+            ]
+        ]
+    ]);
+});
+
+Route::get('/merchants/{id}', function ($id) {
+    return response()->json([
+        'status' => 'success',
+        'merchant' => ['id' => $id]
+    ]);
+});
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,105 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use Agenciamav\LaravelIfood\IfoodAuthorization;
+use Agenciamav\LaravelIfood\IfoodAuthentication;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+
+    $user = request()->user();
+
+    // returns the user code to paste in the ifood portal
+    $user_code = $user->getUserCode();
+    dd($user_code);
+
+    // Get a new access token, passing the authorization code
+    $access_token = $user->getAccessToken('DJSF-PSGD');
+    // dd($access_token);
+
+
+    $merchants = $user->merchants();
+    dd($merchants);
+
+    // if ($ifoodAuthorizationToken == null) {
+    // 	return redirect('/ifood/auth');
+    // }
+
+    // IfoodAuthorization::setAuth($ifoodAuthorizationToken);
+    // $auth       = IfoodAuthorization::getAuth();
+
+    // dd($userCode, $auth);
+
+    // if ($userCode == null) {
+    //     return redirect('/ifood/auth');
+    // }
+
+    // Client (User | Store | Any ... )
+    // $client = request()->user();
+    // $ifoodAuthorizationToken = $client->ifoodAuthorization;
+
+    // $user_code = IfoodAuthorization::getUserCode();
+
+    // dd($user_code);
+
+    // Client has Ifood Access Token ?
+    // if ($ifoodAuthorizationToken == null || $ifoodAuthorizationToken->access_token == null) {
+    //     return redirect('/ifood/auth');
+    // }
+
+    // // Client Ifood Access Token
+    // $merchants1 = $client->merchants();
+    // $merchants2 = Merchant::all();
+    // dd($merchants1, $merchants2, $ifoodAuthorizationToken->access_token);
+
+    // // Client received the USER CODE, now we can request the AUTHORIZATION TOKEN
+    // return Inertia::render('Ifood/Auth', [
+    //     'ifood_authorization_token' => $ifoodAuthorizationToken
+    // ]);
+})->middleware(['auth', 'verified'])->name('ifood');
+
+
+Route::get('/auth', function () {
+    // Client (User | Store | Any ... )
+    $client = request()->user();
+    $ifoodAuthorizationToken = $client->ifoodAuthorization;
+    dd($ifoodAuthorizationToken);
+    $userCode   = IfoodAuthorization::getUserCode();
+
+    dd($ifoodAuthorizationToken, $userCode);
+
+    IfoodAuthorization::setAuth($ifoodAuthorizationToken);
+    $auth       = IfoodAuthorization::getAuth();
+
+    dd($userCode, $auth);
+
+
+    // Client must have valid USER CODE to init the auth process
+    if (
+        $ifoodAuthorizationToken == null || $ifoodAuthorizationToken->user_code == null
+    ) {
+        $client->requestNewUserCode();
+        return redirect('/ifood/auth');
+    }
+
+    // if the client has already received the USER CODE, AUTHORIZATION TOKEN, and ACCESS TOKEN
+    if ($ifoodAuthorizationToken->access_token != null) {
+        // we can redirect to the dashboard
+        return redirect('/ifood');
+    }
+
+    // Client received the USER CODE, now we can request the AUTHORIZATION TOKEN
+    return Inertia::render('Ifood/Auth', [
+        'ifood_authorization_token' => $ifoodAuthorizationToken,
+    ]);
+})->middleware(['auth', 'verified'])->name('ifood');
+
+
+
+Route::get('/ifood/merchants', function () {
+    $client = request()->user();
+    $merchants = $client->merchants();
+    dd($merchants);
+    return Inertia::render('Ifood/Merchants', [
+        'merchants' => $merchants
+    ]);
+})->middleware(['auth', 'verified'])->name('ifood');
